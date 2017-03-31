e3220ec00356666ef09ae1c280aae38e29d85b39
yeasinali <yeasin1994@gmail.com>
2017-03-31 20:48:13 +0600

    Material tabs

---
+++ app/src/main/java/com/food/recipe/simple_tab/TueFragment.java
@@ -0,0 +1,29 @@
+package com.food.recipe.simple_tab;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TueFragment extends Fragment {
+
+    public TueFragment() {
+        // Required empty public constructor
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tue, container, false);
+    }
+
+}
